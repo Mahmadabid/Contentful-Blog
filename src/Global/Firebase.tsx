8ab973d5e4f2e2461180bd4ab4e6523c04e269da
muhammad Ahmad <theahmed157@gmail.com>
2021-04-19 12:52:19 +0500

    Update Firebase.tsx

--- src/Global/Firebase.tsx
+++ src/Global/Firebase.tsx
@@ -1,7 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-const firebaseConfig = {
+const config = {
     apiKey: "AIzaSyAue6tntchg5c8sUxZdneiRkFE3nh4rISU",
     authDomain: "contentful-blog-5ffad.firebaseapp.com",
     projectId: "contentful-blog-5ffad",
@@ -10,8 +10,19 @@ const firebaseConfig = {
     appId: "1:858257695239:web:5d6ce28cb9398e114480a2"
   };
 
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
 
+  let instance: any;
+
+export default function getFirebase() {
+if (typeof window !== 'undefined') {
+if (instance) return instance;
+instance = firebase.initializeApp(config);
+return instance;
+}
+
+return null;
+}
   
 export const auth = firebase.auth();
 

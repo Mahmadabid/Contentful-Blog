40439d13ddcfe73ad80d518aec28e4873b4b520d
muhammad Ahmad <theahmed157@gmail.com>
2021-04-19 10:18:51 +0500

    added some changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import store from './src/Global/store';
 import { Theme } from "./src/Global/theme";
 
+
 export const wrapRootElement = ({ element }) => {
   
   return <Provider store={store}><Theme>{element}</Theme></Provider>;

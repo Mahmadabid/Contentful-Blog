5886e3d4da8ed42bacbfc9bf310bef1abfe6b207
muhammad Ahmad <theahmed157@gmail.com>
2021-04-19 09:40:58 +0500

    Update Firebase.tsx

--- src/Global/Firebase.tsx
+++ src/Global/Firebase.tsx
@@ -1,16 +1,16 @@
-import firebase from 'firebase';
+// import firebase from 'firebase';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAue6tntchg5c8sUxZdneiRkFE3nh4rISU",
-    authDomain: "contentful-blog-5ffad.firebaseapp.com",
-    projectId: "contentful-blog-5ffad",
-    storageBucket: "contentful-blog-5ffad.appspot.com",
-    messagingSenderId: "858257695239",
-    appId: "1:858257695239:web:5d6ce28cb9398e114480a2"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAue6tntchg5c8sUxZdneiRkFE3nh4rISU",
+//     authDomain: "contentful-blog-5ffad.firebaseapp.com",
+//     projectId: "contentful-blog-5ffad",
+//     storageBucket: "contentful-blog-5ffad.appspot.com",
+//     messagingSenderId: "858257695239",
+//     appId: "1:858257695239:web:5d6ce28cb9398e114480a2"
+//   };
 
-  firebase.initializeApp(firebaseConfig);
+//   firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
+// export const auth = firebase.auth();
 
-export const provider = new firebase.auth.GoogleAuthProvider();
+// export const provider = new firebase.auth.GoogleAuthProvider();

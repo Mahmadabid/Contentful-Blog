ab15fc59010ff59ea7c5c57b88fad01beb241af1
muhammad Ahmad <theahmed157@gmail.com>
2021-04-19 10:21:41 +0500

    changed login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -34,11 +34,6 @@ export const LogIn = () => {
                 /** @type {firebase.auth.OAuthCredential} */
                 var credential = result.credential;
 
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                var token = credential.accessToken;
-                // The signed-in user info.
-                var user = result.user;
-                // ...
                 dispatch(setLoggedIn())
             }).catch((error: any) => {
                 console.log(error);;
